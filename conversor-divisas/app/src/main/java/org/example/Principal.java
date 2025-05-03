8acd9541a9462d6ee0ffc1fbc147ba1e6d473465
Juliana Bustos <julibustosar@gmail.com>
2025-05-03 15:48:47 -0500

    se realizó el menú y la interacción con el usuario.

--- conversor-divisas/app/src/main/java/org/example/Principal.java
+++ conversor-divisas/app/src/main/java/org/example/Principal.java
@@ -5,9 +5,64 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
+        int opcion = 0;
+        Scanner teclado = new Scanner(System.in);
         Controlador controlador = new Controlador();
-        //System.out.println(controlador.realizarConversion(30,"USD", "COP"));
 
-        
+        String menu = """
+                ********************************
+                ¡Bienvenido/a al conversor de monedas!
+                
+                Introduzca la opción de conversión a realizar.
+                
+                1) Dólar => Peso argentino.
+                2) Peso argentino => Dólar.
+                3) Dólar => Peso colombiano.
+                4) Peso colombiano => Dólar.
+                5) Peso mexicano => Peso colombiano.
+                6) Peso colombiano => Peso mexicano.
+                7) Salir.
+                ********************************
+                """;
+
+        while (opcion != 7) {
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+
+            if(opcion != 7) {
+                System.out.println("Ingrese la cantidad de dinero a convertir:");
+                int numeroIngresado = teclado.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.println(controlador.realizarConversion(numeroIngresado, "USD", "ARS"));
+                        break;
+
+                    case 2:
+                        System.out.println(controlador.realizarConversion(numeroIngresado, "ARS", "USD"));
+                        break;
+
+                    case 3:
+                        System.out.println(controlador.realizarConversion(numeroIngresado, "USD", "COP"));
+                        break;
+
+                    case 4:
+                        System.out.println(controlador.realizarConversion(numeroIngresado, "COP", "USD"));
+                        break;
+
+                    case 5:
+                        System.out.println(controlador.realizarConversion(numeroIngresado, "MXN", "COP"));
+                        break;
+
+                    case 6:
+                        System.out.println(controlador.realizarConversion(numeroIngresado, "COP", "MXN"));
+                        break;
+
+                    default:
+                        System.out.println("Opción no válida. Introduzca una opción del menú.");
+
+                }
+            }
+        }
     }
 }
